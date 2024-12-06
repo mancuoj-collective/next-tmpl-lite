80833a0863b6551e568f4048b678e8cf7a2fa927
mancuoj <mancuoj@gmail.com>
2024-12-06 19:43:36 +0800

    chore: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,13 @@ import { Button } from '@/components/ui/button'
 
 export default async function Page() {
   return (
-    <div className="flex min-h-svh items-center justify-center gap-5">
-      <Button asChild>
+    <div className="flex min-h-svh items-center justify-center gap-2">
+      <Button size="icon" variant="outline" asChild className="rounded-full">
         <a href="https://github.com/mancuoj-collective/next-tmpl-lite">
           <span className="i-mingcute-github-line" />
-          GitHub
         </a>
       </Button>
-      <DarkModeToggle />
+      <DarkModeToggle className="rounded-full" />
     </div>
   )
 }
--- src/components/theme/dark-mode-toggle.tsx
+++ src/components/theme/dark-mode-toggle.tsx
@@ -1,19 +1,21 @@
 'use client'
 
 import { useDark } from '@/hooks/use-dark'
-import { cn } from '@/lib/utils'
+
+import { Button } from '../ui/button'
 
 export function DarkModeToggle({ className }: { className?: string }) {
   const { toggleDark } = useDark()
 
   return (
-    <button
-      type="button"
+    <Button
+      size="icon"
+      variant="outline"
       onClick={toggleDark}
-      className={cn('inline-flex items-center justify-center', className)}
+      className={className}
     >
       <span className="sun i-mingcute-sun-line rotate-0 scale-100 transition-transform duration-500 dark:-rotate-90 dark:scale-0" />
       <span className="moon i-mingcute-moon-stars-line absolute rotate-90 scale-0 transition-transform duration-500 dark:rotate-0 dark:scale-100" />
-    </button>
+    </Button>
   )
 }
