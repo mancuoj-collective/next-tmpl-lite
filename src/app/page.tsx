808413168d04ec31334f106b9bf96ee4a9f4b253
mancuoj <mancuoj@gmail.com>
2025-05-27 00:34:32 +0800

    feat: shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex flex-col items-center justify-center h-svh">
+      <Button size="lg">Next</Button>
     </div>
   );
 }
