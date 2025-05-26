44636e60677cfd9eb44a3d997d3d0c10c71d3158
mancuoj <mancuoj@gmail.com>
2025-05-27 00:15:27 +0800

    chore: re

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
-import { DarkModeToggle } from '@/components/theme'
-import { Button } from '@/components/ui/button'
-
-export default async function Page() {
+export default function Home() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-5">
-      <div className="font-dm text-2xl font-semibold">Next - Tmpl - Lite</div>
-      <div className="space-x-2">
-        <Button variant="outline" size="icon" asChild className="rounded-full">
-          <a href="https://github.com/mancuoj-collective/next-tmpl-lite">
-            <span className="i-carbon-logo-github" />
-          </a>
-        </Button>
-        <DarkModeToggle className="rounded-full" />
-      </div>
+    <div>
+      <h1>Hello World</h1>
     </div>
-  )
+  );
 }
--- src/components/theme/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './dark-mode-toggle'
-export * from './theme-script'
-export * from './tw-screen-indicator'
