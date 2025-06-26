7e74dc76c91b12cf606e4e60de464482dc1ef752
mancuoj <mancuoj@gmail.com>
2025-06-26 22:50:39 +0800

    chore: maybe i donot need eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { ThemeProvider } from 'next-themes'
-import { IBM_Plex_Mono, Libre_Baskerville, Lora } from 'next/font/google'
+import { Libre_Baskerville } from 'next/font/google'
 import { Toaster } from '@/components/ui/sonner'
 import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
@@ -11,24 +11,9 @@ const fontSans = Libre_Baskerville({
   weight: ['400', '700'],
 })
 
-const fontMono = IBM_Plex_Mono({
-  variable: '--font-mono',
-  subsets: ['latin'],
-  weight: ['400', '700'],
-})
-
-const fontSerif = Lora({
-  variable: '--font-serif',
-  subsets: ['latin'],
-})
-
 export const metadata: Metadata = {
   title: 'Next',
   description: 'Next.js template lite version',
-  icons: {
-    icon: '/next.svg',
-    shortcut: '/next.svg',
-  },
 }
 
 export default function RootLayout({
@@ -38,14 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          fontSans.variable,
-          fontMono.variable,
-          fontSerif.variable,
-          'font-sans antialiased',
-        )}
-      >
+      <body className={cn(fontSans.variable, 'font-sans antialiased')}>
         <ThemeProvider attribute="class" storageKey="next-tmpl-theme" disableTransitionOnChange>
           {children}
           <Toaster />
